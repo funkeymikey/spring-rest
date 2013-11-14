85446cd372a20d9292614605bcf05e2441bebdd0
Mike Kozelsky <mike.kozelsky@gmail.com>
2013-11-14 16:15:28 -0500

    basically working

--- src/main/java/com/example/controller/Person.java
+++ src/main/java/com/example/controller/Person.java
@@ -1,15 +1,11 @@
 package com.example.controller;
 
 public class Person {
-	private String name;
-	
-	public String getName() {
-		return name;
+	public String name;
+	public int age;
+		
+	@Override
+	public String toString() {
+		return name + "@" + age;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
 }
--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -1,8 +1,12 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,43 +15,74 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("person")
 public class PersonController {
 
-//    private List<Person> names = Arrays.asList(new Person("name one"), new Person("name two"), new Person("name three"));
-// 
-//    /**
-//     * GET /person
-//     * @return
-//     */
-//    @RequestMapping(method = RequestMethod.GET)
-//    @ResponseBody
-//    public ResponseEntity<List<Person>> query() {
-//        return new ResponseEntity<List<Person>>(names, HttpStatus.OK);
-//    }
-// 
-//    /**
-//     * GET /person/{id}
-//     * @param id
-//     * @return
-//     */
-//    @RequestMapping(value="{id}", method = RequestMethod.GET)
-//    @ResponseBody
-//    public ResponseEntity<Person> get(@PathVariable Long id) {
-//    	if(id >= names.size()){
-//    		return new ResponseEntity<Person>(null, null, HttpStatus.NOT_FOUND);
-//    	}
-//    	
-//    	Person value = names.get(id.intValue());
-//    	return new ResponseEntity<Person>(value, HttpStatus.OK);
-//    }
+    private List<Person> peeps = new ArrayList<Person>();
+ 
+    public PersonController(){
+    	Person p;
+    	
+    	p = new Person();
+    	p.name = "name one";
+    	p.age = 48;
+    	peeps.add(p);
+    	
+    	p = new Person();
+    	p.name = "name two";
+    	p.age = 39;
+    	peeps.add(p);
+    	
+    	p = new Person();
+    	p.name = "name three";
+    	p.age = 92;
+    	peeps.add(p);    	
+    }
+    
+    /**
+     * GET /person
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<List<Person>> query() {
+        return new ResponseEntity<List<Person>>(peeps, HttpStatus.OK);
+    }
+ 
+    /**
+     * GET /person/{id}
+     * @param id
+     * @return
+     */
+    @RequestMapping(value="{personId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Person> get(@PathVariable final Long personId) {
+    	if(personId >= peeps.size()){
+    		return new ResponseEntity<Person>(null, null, HttpStatus.NOT_FOUND);
+    	}
+    	
+    	Person value = peeps.get(personId.intValue());
+    	return new ResponseEntity<Person>(value, HttpStatus.OK);
+    }
 
     /**
      * POST /person
-     * @param person
+     * @param p
      * @return
      */
     @RequestMapping(method=RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Person> create(Person person) {
-    	System.out.println("name4: "+person.getName());
-        return new ResponseEntity<Person>(person, HttpStatus.CREATED);
+    public ResponseEntity<Person> create(@RequestBody final Person p) {
+    	System.out.println("person: " + p);
+        return new ResponseEntity<Person>(p, HttpStatus.CREATED);
+    }
+    
+    /**
+     * PUT /person
+     * @param p
+     * @return
+     */
+    @RequestMapping(value="{personId}", method=RequestMethod.PUT)
+    @ResponseBody
+    public ResponseEntity<Person> update(@PathVariable final Long personId, @RequestBody final Person p) {
+    	System.out.println("updating person: " + personId +" to " + p);    	
+        return new ResponseEntity<Person>(p, HttpStatus.OK);
     }
 }
